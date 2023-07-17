8988bd5216d2fd0b923ff84c5e4a8f585bd2c5db
PrAndrian <princydruh@gmail.com>
2023-07-17 19:11:49 +0200

    clean up + Navbar debut

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import styles from '../styles/Navbar.module.css'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav id={styles.navbar}>
+      <Link to="/">
+        <img className={styles.logo} src={logo} alt="SportSee_logo"/>
+      </Link>
+
+      <div className={styles.links}>
+        <NavLink to="/">
+          Accueil
+        </NavLink>
+        <NavLink to="/">
+          Profil
+        </NavLink>
+        <NavLink to="/">
+          Réglage
+        </NavLink>
+        <NavLink to="/">
+          Communauté
+        </NavLink>
+      </div>
+
+      
+    </nav>
   )
 }
 
